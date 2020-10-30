f5abe2df9caf012050e14bb24cde41f2477a8125
Paul Ffrench <paul@brickfieldlabs.ie>
2020-10-30 13:38:49 +0000

    Code cleanup 2, mform replaced textarea input

--- index.php
+++ index.php
@@ -27,6 +27,10 @@
  */
 
 require_once(__DIR__. '/../../config.php');
+require_once(__DIR__ . '/classes/form.php');
+
+$inputform = optional_param('text', 0, PARAM_TEXT);
+//$deleteform = optional_param('submit', 0, PARAM_INT);
 
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url(new moodle_url('/local/helloworld/index.php'));
@@ -40,62 +44,52 @@ if (isguestuser()) {
 }
 
 echo $OUTPUT->header();
-?>
-    <form method="post" enctype="multipart/form-data">
-        <?php if (has_capability('local/helloworld:postmessages', context_system::instance())) { ?>
-            <div class="form-group"> <textarea class="form-control" name="PostDescription"  rows="4" cols="100" placeholder="Type a message" value="' . s($PostDescription) . '"></textarea> </div>         
-            <button type='submit' name='Submitpost'>Submit </button>
-         <?php } ?>
-    </form>
+$mform = new inputform();
+$mform->display();
 
-    <!-- Input of records -->
+//$deleteform = new deleteform();
+
+if(!empty($inputform)) {
+    $User_name = $DB->get_record('user', [ 'id' => $USER->id ]);
+    $record = new stdClass(); 
+    $record->timecreated = time();
+    $record->message = $inputform;
+    $record->userid = $User_name->id;
+    require_capability('local/helloworld:viewmessages', context_system::instance());
+    $lastinsertid = $DB->insert_record('local_helloworld_msgs', $record, false);
+}
+?>
+<div class="row">
     <?php
-    if (isset($_POST["Submitpost"]) && !empty($_POST["PostDescription"])) {
-        $User_name = $DB->get_record('user', [ 'id' => $USER->id ]);
-        $record = new stdClass(); 
-        $record->timecreated = time();
-        $record->message = $_POST["PostDescription"];
-        $record->userid = $User_name->id;
-        require_capability('local/helloworld:viewmessages', context_system::instance());
-            $lastinsertid = $DB->insert_record('local_helloworld_msgs', $record, false);
-    } else if(isset($_POST["Submitpost"]) && empty($_POST["PostDescription"])) {
-        echo "Please enter some text";
-    }
+    $sql = "SELECT * FROM m_user JOIN m_local_helloworld_msgs ON m_local_helloworld_msgs.userid = m_user.id";
+    $usercomment = $DB->get_records_sql($sql);
+    foreach ($usercomment as $User_comment) {
+    $timesincepost = $User_comment->timecreated - time();
     ?>
-    <!-- Display of records -->
-    <div class="row">
-        <?php 
-        global $DB; 
-        $sql = "SELECT * FROM m_user JOIN m_local_helloworld_msgs ON m_local_helloworld_msgs.userid = m_user.id";
-        $usercomment = $DB->get_records_sql($sql);
-        foreach ($usercomment as $User_comment) {
-        ?>
-        <div class="col-sm-4" style="padding-bottom: 10px;">
-            <div class="card">
-                <div class="card-header"> <?php echo $converted_name = fullname($User_comment); ?> </div>
-                <div class="card-body"> <p class="card-text"><?php echo format_string($User_comment->message) ?></p> </div>
-                <div class="card-footer text-muted">
-                    <?php $timesincepost = $User_comment->timecreated - time(); echo format_time($timesincepost); ?>
-                    <div class="float-right">
-                        <?php if (has_capability('local/helloworld:deleteanymessage', context_system::instance())) { ?>
-                            <form action="local/helloworld/index.php" method="post">
-                                <input type="checkbox" name="Deletepost" />
-                                <input type="submit" name="formSubmit" value="Delete" />
-                                <input class="hidden" type="text" name="postid" value="<?php echo $User_comment->id; ?>">
-                            </form>
-                        <?php } ?>
-                    </div>
-                </div>   
-            </div>               
-        </div>
-        <?php } ?>
-        <?php 
-            if(isset($_POST['Deletepost'])) {
-            $usercommentid = $_POST["postid"]; 
-            $DB->delete_records('local_helloworld_msgs', [ 'id' => $usercommentid ]);
-            }
-        ?>  
+    <div class="col-sm-4" style="padding-bottom: 10px;">
+        <div class="card">
+            <div class="card-header"> <?php echo $converted_name = fullname($User_comment); ?> </div>
+            <div class="card-body"> <p class="card-text"><?php echo format_string($User_comment->message) ?></p> </div>
+            <div class="card-footer text-muted"> <?php  echo format_time($timesincepost); ?>
+                <div class="float-right">
+                    <?php if (has_capability('local/helloworld:deleteanymessage', context_system::instance())) { ?>
+                        <form action="index.php" method="post">
+                            <input type="checkbox" name="Deletepost" />
+                            <input type="submit" name="formSubmit" value="Delete" />
+                            <input class="hidden" type="text" name="postid" value="<?php echo $User_comment->id; ?>">
+                        </form>
+                    <?php } ?>
+                </div>
+            </div>   
+        </div>               
     </div>
+    <?php } 
+    if(isset($_POST['Deletepost'])) {
+        $usercommentid = $_POST["postid"]; 
+        $DB->delete_records('local_helloworld_msgs', [ 'id' => $usercommentid ]);
+    } 
+    ?>
+</div>
     
 <?php echo $OUTPUT->footer(); ?>
 
